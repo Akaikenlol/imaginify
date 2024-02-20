129e07bc0ae75f18c5417c982c11d9487ebd212b
Akaikenlol <keithken972@gmail.com>
2024-02-20 18:39:24 +0630

    chore: typo fix

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MongoDB_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
 	conn: Mongoose | null;
