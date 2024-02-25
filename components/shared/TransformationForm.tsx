328ee06b2770836ed639810c09c6177ad497f920
Akaikenlol <keithken972@gmail.com>
2024-02-25 20:07:29 +0630

    feat: Transformation Image Server chore: npm alert dialog

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useTransition } from "react";
+import React, { useEffect, useState, useTransition } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -26,6 +26,10 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
 import TransformImage from "./TransformImage";
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModel";
 
 export const formSchema = z.object({
 	title: z.string(),
@@ -51,6 +55,7 @@ const TransformationForm = ({
 	const [isTransforming, setIsTransforming] = useState(false);
 	const [transformationConfig, setTransformationConfig] = useState(config);
 	const [isPending, startTransition] = useTransition();
+	const router = useRouter();
 	const initialValues =
 		data && action === "Update"
 			? {
@@ -66,8 +71,71 @@ const TransformationForm = ({
 		defaultValues: initialValues,
 	});
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
+	async function onSubmit(values: z.infer<typeof formSchema>) {
 		console.log(values);
+
+		setIsSubmitting(true);
+
+		if (data || image) {
+			const transformationUrl = getCldImageUrl({
+				width: image?.width,
+				height: image?.height,
+				src: image?.publicId,
+				...transformationConfig,
+			});
+
+			const imageData = {
+				title: values.title,
+				publicId: image?.publicId,
+				transformationType: type,
+				width: image?.width,
+				height: image?.height,
+				config: transformationConfig,
+				secureURL: image?.secureURL,
+				transformationURL: transformationUrl,
+				aspectRatio: values.aspectRatio,
+				prompt: values.prompt,
+				color: values.color,
+			};
+
+			if (action === "Add") {
+				try {
+					const newImage = await addImage({
+						image: imageData,
+						userId,
+						path: "/",
+					});
+
+					if (newImage) {
+						form.reset();
+						setImage(data);
+						router.push(`/transformations/${newImage._id}`);
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			}
+
+			if (action === "Update") {
+				try {
+					const updatedImage = await updateImage({
+						image: {
+							...imageData,
+							_id: data._id,
+						},
+						userId,
+						path: `/transformations/${data._id}`,
+					});
+
+					if (updatedImage) {
+						router.push(`/transformations/${updatedImage._id}`);
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		}
+		setIsSubmitting(false);
 	}
 
 	const onSelectFieldHandler = (
@@ -105,7 +173,6 @@ const TransformationForm = ({
 		}, 1000);
 	};
 
-	// TO DO: Update creditFee to something else
 	const onTransformHandler = async () => {
 		setIsTransforming(true);
 
@@ -115,15 +182,22 @@ const TransformationForm = ({
 
 		setNewTransformation(null);
 		startTransition(async () => {
-			await updateCredits(userId, -1);
+			await updateCredits(userId, creditFee);
 		});
 	};
 
 	const onValueChange = () => {};
 
+	useEffect(() => {
+		if (image && (type === "removeBackground" || type === "restore")) {
+			setNewTransformation(transformationType.config);
+		}
+	}, [image, type, transformationType.config]);
+
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				{creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
 				<CustomField
 					control={form.control}
 					name={"title"}
--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,7 +4,7 @@ export interface IImage extends Document {
 	title: string;
 	transformationType: string;
 	publicId: string;
-	secureUrl: string;
+	secureURL: string;
 	width?: number;
 	height?: number;
 	config?: object;
@@ -25,11 +25,11 @@ const ImageSchema = new Schema({
 	title: { type: String, required: true },
 	transformationType: { type: String, required: true },
 	publicId: { type: String, required: true },
-	secureUrl: { type: String, required: true },
+	secureURL: { type: String, required: true },
 	width: { type: Number },
 	height: { type: Number },
 	config: { type: Object },
-	transformationUrl: { type: URL },
+	transformationUrl: { type: String },
 	aspectRation: { type: String },
 	color: { type: String },
 	prompt: { type: String },
